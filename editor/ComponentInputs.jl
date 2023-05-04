9a60313e803ef2036770f419983e9528910d6370
Kyle Conel <kyjordev@gmail.com>
2023-05-04 08:25:20 -0400

    Bool and string for components editable

--- editor/ComponentInputs.jl
+++ editor/ComponentInputs.jl
@@ -44,6 +44,25 @@ function ShowComponentPropertyInput(currentEntitySelected, component, componentF
         @c CImGui.InputFloat("$(componentField)x", &x, 1)
         @c CImGui.InputFloat("$(componentField)y", &y, 1)
         currentEntitySelected.getComponent(componentTypeString).setVector2fValue(componentField,convert(Float64, x),convert(Float64, y))
+
+    elseif componentFieldType == "Bool" 
+        @c CImGui.Checkbox("$(componentField)", &componentFieldValue)
+        setfield!(currentEntitySelected.getComponent(componentTypeString),componentField,componentFieldValue)
+
+    elseif componentFieldType == "String"
+        buf = "$(componentFieldValue)"*"\0"^(64)
+        CImGui.InputText("$(componentField)", buf, length(buf))
+        currentTextInTextBox = ""
+        for characterIndex = 1:length(buf)
+            if Int(buf[characterIndex]) == 0 
+                if characterIndex != 1
+                    currentTextInTextBox = String(SubString(buf, 1, characterIndex-1))
+                end
+                break
+            end
+        end
+        setfield!(currentEntitySelected.getComponent(componentTypeString),componentField,componentFieldValue)
+
     end
 
 end
