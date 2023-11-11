8135513eb810cda24df32f7a697b1d03d8dba2f0
Kyle Conel <kyjordev@gmail.com>
2023-11-11 00:08:47 -0500

    Remove extra reference to sdl2

--- src/UI/ScreenButton.jl
+++ src/UI/ScreenButton.jl
@@ -1,7 +1,6 @@
 module ScreenButtonModule    
     using ..UI.JulGame
     using ..UI.JulGame.Math
-    using SimpleDirectMediaLayer.LibSDL2
 
     export ScreenButton
     mutable struct ScreenButton
@@ -21,8 +20,8 @@ module ScreenButtonModule
         function ScreenButton(dimensions::Math.Vector2, position::Math.Vector2, buttonUpSprite, buttonDownSprite, text)
             this = new()
             
-            this.buttonDownSprite = IMG_Load(buttonDownSprite)
-            this.buttonUpSprite = IMG_Load(buttonUpSprite)
+            this.buttonDownSprite = SDL2.IMG_Load(buttonDownSprite)
+            this.buttonUpSprite = SDL2.IMG_Load(buttonUpSprite)
             this.clickEvents = []
             this.dimensions = dimensions
             this.mouseOverSprite = false
@@ -39,25 +38,25 @@ module ScreenButtonModule
                 if !this.mouseOverSprite && this.currentTexture == this.buttonDownTexture
                     this.currentTexture = this.buttonUpTexture
                 end    
-                @assert SDL_RenderCopyEx(
+                @assert SDL2.SDL_RenderCopyEx(
                     this.renderer, 
                     this.currentTexture, 
                     C_NULL, 
-                    Ref(SDL_Rect(this.position.x, this.position.y, this.dimensions.x,this.dimensions.y)), 
+                    Ref(SDL2.SDL_Rect(this.position.x, this.position.y, this.dimensions.x,this.dimensions.y)), 
                     0.0, 
                     C_NULL, 
                     SDL_FLIP_NONE) == 0 "error rendering image: $(unsafe_string(SDL_GetError()))"
 
-                @assert SDL_RenderCopy(this.renderer, this.textTexture, C_NULL, Ref(SDL_Rect(this.position.x + 50, this.position.y + 10,150,50))) == 0 "error rendering button text: $(unsafe_string(SDL_GetError()))"
+                @assert SDL2.SDL_RenderCopy(this.renderer, this.textTexture, C_NULL, Ref(SDL2.SDL_Rect(this.position.x + 50, this.position.y + 10,150,50))) == 0 "error rendering button text: $(unsafe_string(SDL2.SDL_GetError()))"
             end
         elseif s == :injectRenderer
             function(renderer, font)
                 this.renderer = renderer
-                this.buttonDownTexture = SDL_CreateTextureFromSurface(this.renderer, this.buttonDownSprite)
-                this.buttonUpTexture = SDL_CreateTextureFromSurface(this.renderer, this.buttonUpSprite)
+                this.buttonDownTexture = SDL2.SDL_CreateTextureFromSurface(this.renderer, this.buttonDownSprite)
+                this.buttonUpTexture = SDL2.SDL_CreateTextureFromSurface(this.renderer, this.buttonUpSprite)
                 this.currentTexture = this.buttonUpTexture
-                text = TTF_RenderText_Blended(font, this.text, SDL_Color(255,255,255,255) )
-                this.textTexture = SDL_CreateTextureFromSurface(this.renderer, text)
+                text = SDL2.TTF_RenderText_Blended(font, this.text, SDL2.SDL_Color(255,255,255,255) )
+                this.textTexture = SDL2.SDL_CreateTextureFromSurface(this.renderer, text)
 
             end
         elseif s == :setPosition
@@ -69,14 +68,14 @@ module ScreenButtonModule
             end
         elseif s == :handleEvent
             function(evt, x, y)
-                if evt.type == evt.type == SDL_MOUSEBUTTONDOWN
+                if evt.type == evt.type == SDL2.SDL_MOUSEBUTTONDOWN
                     this.currentTexture = this.buttonDownTexture
-                elseif evt.type == SDL_MOUSEBUTTONUP
+                elseif evt.type == SDL2.SDL_MOUSEBUTTONUP
                     this.currentTexture = this.buttonUpTexture
                     for eventToCall in this.clickEvents
                         eventToCall()
                     end
-                elseif evt.type == SDL_MOUSEMOTION
+                elseif evt.type == SDL2.SDL_MOUSEMOTION
                     #println("mouse move")
                 end 
             end
