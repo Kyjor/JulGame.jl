5ed93eb24a4041ac2bde155131beb48c94929d3d
Kyle Conel <kyjordev@gmail.com>
2023-11-26 15:08:02 -0500

    Trying to create a scene in test

--- test/engine/engine.jl
+++ test/engine/engine.jl
@@ -1,7 +1,10 @@
+using JulGame
 using Test
 
 
 
 @testset "Example" begin
     @test 1 == 2
+    scene = SceneBuilderModule.Scene(joinpath(pwd(), ".."), "scene.json")
+    main = scene.init("JulGame Example", false, Math.Vector2(1920, 1080),Math.Vector2(576, 576), false, 1.0, true)
 end
