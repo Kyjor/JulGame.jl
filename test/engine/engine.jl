102bd85229bab4cd73081f3dc0da77e6ab4ad62e
Kyle Conel <kyjordev@gmail.com>
2023-11-29 20:51:04 -0500

    Fix test after merge

--- test/engine/engine.jl
+++ test/engine/engine.jl
@@ -1,5 +1,6 @@
 @testset "Open and close" begin
     JulGame.MAIN.testMode = true
-    scene = SceneBuilderModule.Scene(joinpath(ROOTDIR , "examples", "Testing", "Testing"), "scene.json")
+    scene = SceneBuilderModule.Scene(joinpath(ROOTDIR , "examples", "Testing", "Testing","scenes", "scene.json"))
+    JulGame.BasePath = joinpath(ROOTDIR , "examples", "Testing", "Testing")
     main = scene.init("JulGame Example", false, Math.Vector2(1920, 1080),Math.Vector2(576, 576), false, 1.0, true)
 end
