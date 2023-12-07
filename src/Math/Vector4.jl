c07bbb7365280147d4e654b4162cdeab9b340ce8
Kyle Conel <kyjordev@gmail.com>
2023-12-06 20:33:08 -0500

    Crop variable is never null

--- src/Math/Vector4.jl
+++ src/Math/Vector4.jl
@@ -7,6 +7,7 @@ struct Vector4
     h::Int64
     #default constructor
     Vector4() = new(0, 0, 0, 0)
+    Vector4(num::Number) = new(convert(Int64,num), convert(Int64,num), convert(Int64,num), convert(Int64,num))
     Vector4(x::Int64, y::Int64) = new(x, y, x, y)
     Vector4(x::Int64, y::Int64, w::Int64, h::Int64) = new(x,y,w,h)
     Vector4(x::Number, y::Number, w::Number, h::Number) = new(convert(Int64,x),convert(Int64,y),convert(Int64,w),convert(Int64,h))
