358ef1e0d00b6792a107244f43cf5421ed1c4b03
Kyle Conel <kyjordev@gmail.com>
2024-01-03 21:10:12 -0500

    Constructor tests added

--- test/projects/SmokeTest/scripts/TestScript.jl
+++ test/projects/SmokeTest/scripts/TestScript.jl
@@ -1,6 +1,11 @@
 using JulGame.AnimationModule
-using JulGame.Component 
-using JulGame.EntityModule 
+using JulGame.CircleColliderModule
+using JulGame.ColliderModule
+using JulGame.RigidbodyModule
+using JulGame.ShapeModule
+using JulGame.SoundSourceModule
+using JulGame.SpriteModule
+using JulGame.TransformModule 
 using JulGame.MainLoop 
 using JulGame.Math
 using JulGame.UI
@@ -30,6 +35,26 @@ function Base.getproperty(this::TestScript, s::Symbol)
                 @testset "Animator constructor" begin
                     newAnimator = AnimatorModule.Animator(AnimationModule.Animation[newAnimation])
                     @test newAnimator != C_NULL && newAnimator !== nothing
+                end
+
+                @testset "CircleCollider constructor" begin
+                    newCircleCollider = CircleColliderModule.CircleCollider(1.0, true, false, Math.Vector2f(0,0), "Default")
+                    @test newCircleCollider != C_NULL && newCircleCollider !== nothing
+                end
+
+                @testset "Collider constructor" begin
+                    newCollider = ColliderModule.Collider(true, false, false, Math.Vector2f(0,0), Math.Vector2f(1,1), "Default")
+                    @test newCollider != C_NULL && newCollider !== nothing
+                end
+
+                @testset "Rigidbody constructor" begin
+                    newRigidbody = RigidbodyModule.Rigidbody(1.0)
+                    @test newRigidbody != C_NULL && newRigidbody !== nothing
+                end
+
+                @testset "Shape constructor" begin
+                    newShape = ShapeModule.Shape(Math.Vector3(255,0,0), Math.Vector2f(1,1), true, true, Math.Vector2f(0,0), Math.Vector2f(0,0))
+                    @test newShape != C_NULL && newShape !== nothing
                 end
             end
         end
