295c0ecd8b9c4d7c6de5f7529ed0dafc943fd430
Kyle Conel <kyjordev@gmail.com>
2024-01-20 11:08:51 -0500

    Removed the editor from the engine. This should make game builds smaller

---
+++ src/editor/JulGameEditor/precompile_app.jl
@@ -0,0 +1,3 @@
+using JulGameEditor
+
+JulGameEditor.julia_main()
--- src/editor/JulGameEditor/src/JulGameEditor.jl
+++ src/editor/JulGameEditor/src/JulGameEditor.jl
@@ -1,11 +1,11 @@
-module Editor
-    using JulGame
-
+module JulGameEditor
+    include("../Editor.jl")
+    
     function run()
-       JulGame.Editor.run()
+       Editor.run()
     end
 
     julia_main() = run()
 end
 # Uncommented to allow for direct execution of this file. If you want to build this project with PackageCompiler, comment the line below
-Editor.run()
+#JulGameEditor.run()
