c563454187a14e32a6f9813bcc7cae0881d9d849
Kyle Conel <kyjordev@gmail.com>
2024-02-02 22:05:00 -0500

    Fixing text input

--- src/editor/JulGameEditor/Components/TextInputs.jl
+++ src/editor/JulGameEditor/Components/TextInputs.jl
@@ -1,17 +1,11 @@
 
 function text_input_single_line(name::String, currentText::String, filter = nothing) #Todo: add type to filter
-    @cstatic begin
-        buf = "$(currentText)"*"\0"^(128)
-        CImGui.InputText(name, buf, length(buf))
-        for characterIndex = 1:length(buf)
-            if Int32(buf[characterIndex]) == 0 
-                if characterIndex != 1
-                    currentText = String(SubString(buf, 1, characterIndex-1))
-                end
-                break
-            end
-        end
-
-        return currentText
+    @cstatic bufpass="password123"*"\0"^53 begin
+    println("Text input single line")
+    println("bufpass: ", bufpass)
+        CImGui.InputText("password", bufpass, 64, CImGui.ImGuiInputTextFlags_Password | CImGui.ImGuiInputTextFlags_CharsNoBlank)
+        CImGui.SameLine()
+        ShowHelpMarker("Display all characters as '*'.\nDisable clipboard cut and copy.\nDisable logging.\n")
+        CImGui.InputText("password (clear)", bufpass, 64, CImGui.ImGuiInputTextFlags_CharsNoBlank)
     end
 end
