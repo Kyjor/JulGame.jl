0be75dd3d79ece6b7b2579e9f20d690a90a185a3
Kyle Conel <kyjordev@gmail.com>
2024-02-08 21:12:33 -0500

    Added back smoke test and removed platformer

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,11 +9,11 @@ include(joinpath(SMOKETESTDIR, "src", "SmokeTest.jl"))
 include(joinpath(PROFILINGTESTDIR, "Platformer", "src", "Platformer.jl"))
 
 @testset "All tests" begin
-    # cd(joinpath(SMOKETESTDIR, "src"))
-    # @test SmokeTest.run(SMOKETESTDIR) == 0
-    cd(joinpath(@__DIR__, "projects", "ProfilingTest", "Platformer", "src"))
+    cd(joinpath(SMOKETESTDIR, "src"))
+    @test SmokeTest.run(SMOKETESTDIR) == 0
     include("math/mathtests.jl")
-    @testset "Platformer" begin
-        @test Platformer.run() == 0
-    end
+    #cd(joinpath(@__DIR__, "projects", "ProfilingTest", "Platformer", "src"))
+    # @testset "Platformer" begin
+    #     @test Platformer.run() == 0
+    # end
 end
