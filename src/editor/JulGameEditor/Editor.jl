4e9f8490d345e6b22e6636cdbb042e23faabcbaa
Kyle Conel <kyjordev@gmail.com>
2024-03-30 09:39:09 -0700

    We can load fonts in the editor now (#66)

--- src/editor/JulGameEditor/Editor.jl
+++ src/editor/JulGameEditor/Editor.jl
@@ -305,12 +305,11 @@ module Editor
                                 end
                                 CImGui.PopID()
                                 CImGui.Separator()
-                                for field in fieldnames(UI.TextBoxModule.TextBox)
-                                    if length(currentSceneMain.scene.textBoxes) < uiElementIndex
-                                        break
-                                    end
-                                    show_field_editor(currentSceneMain.scene.textBoxes[uiElementIndex], field)
+
+                                if length(currentSceneMain.scene.textBoxes) < uiElementIndex
+                                    break
                                 end
+                                show_textbox_fields(currentSceneMain.scene.textBoxes[uiElementIndex])
 
                                 CImGui.Separator()
                                 if CImGui.Button("Duplicate") 
