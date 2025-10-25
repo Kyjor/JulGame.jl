1ea495c893c1acc877add6b853173b073292ee32
Kyle Conel <kyjordev@gmail.com>
2025-10-24 22:13:22 -0400

    add test length env var

--- src/engine/SceneManagement/SceneBuilder.jl
+++ src/engine/SceneManagement/SceneBuilder.jl
@@ -64,7 +64,7 @@ module SceneBuilderModule
 
         JulGame.MAIN = main
         MAIN.testMode = get(ENV, "TEST_MODE", "false") == "true"
-        MAIN.testLength = 20.0
+        MAIN.testLength = parse(Float64, get(ENV, "TEST_LENGTH", "20.0"))
         MAIN.currentTestTime = 0.0
         MAIN.level = this
         MAIN.scene.name = split(this.scene, ".")[1]
